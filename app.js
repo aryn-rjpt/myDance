df8d58ffb29d57a9713892428f40a79023a9df4b
Aryan <aryanrajput039@gmail.com>
2023-04-02 15:34:22 +0530

    changes port

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express')
 
 const app = express()
-const port = 3000
+// const port = 3000
 
 app.get("/", (req, res) => {
     res.send("This is starting");
 })
 
-app.listen(port, () => {
-    console.log(`Serving at http://localhost:${port}`)
-})
+app.listen(port)
 
 

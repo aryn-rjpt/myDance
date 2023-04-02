56a34152753d85c1529fb14aeb154749fa3b7744
Aryan <aryanrajput039@gmail.com>
2023-04-02 16:17:20 +0530

    port 3000

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const port = 80
+const port = 3000
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view-engine', 'pug')

c3846d85f4082804790ce2d052f9a7ba8c3b19d6
Aryan <aryanrajput039@gmail.com>
2023-04-02 16:51:54 +0530

    contact page

--- app.js
+++ app.js
@@ -7,12 +7,21 @@ const port = 3000
 app.set('views', path.join(__dirname, 'views'))
 app.set('view-engine', 'pug')
 app.use('/static', express.static('static'))
-// app.use(express.urlencoded())
 
 
 
 app.get("/", (req, res) => {
     res.status(200).render('home.pug');
+})
+
+
+app.get('/contact', (req, res)=>{
+    res.status(200).render('contact.pug')
+});
+
+app.post('/contact', (req, res)=>{
+    let status = true;
+    res.status(200).render('contact.pug', {'status':status})
 })
 
 app.listen(port, ()=>{
